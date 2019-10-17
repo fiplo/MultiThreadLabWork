59350850f27481c1f0cc7c34e4969e69d77b68c7
fiplo <pauliuz95@gmail.com>
2019-10-17 13:59:16 +0300

    wtf code

--- Lab1a/src/main.go
+++ Lab1a/src/main.go
@@ -17,16 +17,31 @@ import (
 	"regexp"
 	"sort"
 	"sync"
+	"time"
 )
 
 var (
-	inputMutex  sync.Mutex
-	outputMutex sync.Mutex
-	ageReq      int
+	ageReq int
 )
 
 func init() {
 	ageReq = 30
+}
+
+func work(input *Monitor, output *Monitor, wg *sync.WaitGroup) {
+	var work bool = true
+	defer wg.Done()
+	var user User
+	for work {
+		user = input.takeEntry()
+		if Math.IsNaN(user.Balance) {
+			work = false
+		}
+		if user.Age > ageReq && !IsNaN(user.Balance) {
+			user.Balance = user.Balance * 1.1
+			output.addEntry(user)
+		}
+	}
 }
 
 func main() {
@@ -37,32 +52,25 @@ func main() {
 	var tCount int
 	tCount = 4 // Count of filter threads
 	wg.Add(tCount)
+
+	var inputM Monitor
+	inputM.setSize(10)
+	var outputM Monitor
+	outputM.setSize(users.Len())
+
 	for i := 0; i < tCount; i++ {
-		go func() {
-			var work bool = true
-			var user User
-			for work {
-				inputMutex.Lock()
-				if users.Len() > 0 {
-					user = users.removeLast()
-				} else {
-					work = false
-				}
-				inputMutex.Unlock()
-				if user.Age > ageReq {
-					outputMutex.Lock()
-					users2.addUser(user)
-					outputMutex.Unlock()
-				}
-			}
-			wg.Done()
-		}()
+		go work(&inputM, &outputM, &wg)
+	}
+
+	for i := 0; i < len(users.Users); i++ {
+		inputM.addEntry(users.removeLast())
 	}
 
 	wg.Wait()
 	var outputFile string
-	reg := regexp.MustCompilePOSIX(`dat_.\.json`)
-	outputFile = reg.ReplaceAllString(inputFile, "res.json")
+	reg := regexp.MustCompilePOSIX(".json")
+	outputFile = reg.ReplaceAllString(inputFile, "Res.json")
+
 	output, _ := json.MarshalIndent(users2, " ", "		")
 	outputString := string(output)
 
@@ -144,5 +152,50 @@ func (a Users) removeLast() User {
 }
 
 type Monitor struct {
-	Users []User
+	users   Users
+	mutex   sync.Mutex
+	maxSize int
+	index   int
+	done    bool
+}
+
+func (a Monitor) setSize(size int) {
+	a.maxSize = size
+	a.done = false
+	a.index = 0
+}
+
+func (a Monitor) addEntry(user User) {
+	a.mutex.Lock()
+	for a.users.Len() >= a.maxSize {
+		a.mutex.Unlock()
+		time.Sleep(2 * time.Millisecond)
+		a.mutex.Lock()
+	}
+	a.users.addUser(user)
+	a.index++
+	a.mutex.Unlock()
+}
+
+func (a Monitor) takeEntry() User {
+	var user User
+	a.mutex.Lock()
+	for a.index == 0 {
+		if a.done {
+			a.mutex.Unlock()
+			user.Balance = NaN()
+			return user
+		}
+		a.mutex.Unlock()
+		time.Sleep(2 * time.Millisecond)
+		a.mutex.Lock()
+	}
+	user = a.users.removeLast()
+	a.index--
+	a.mutex.Unlock()
+	return user
+}
+
+func (a Monitor) returnUsers() Users {
+	return a.users
 }
