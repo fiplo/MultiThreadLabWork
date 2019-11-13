e45f8dc6e4cf07ace1e0ef635ff2dde6f75fa7fc
Fiplo <pauliuz95@gmail.com>
2019-11-14 00:31:47 +0200

    Lab2 done

--- Lab2/src/main.go
+++ Lab2/src/main.go
@@ -24,6 +24,51 @@ const (
 	threadCount int = 4
 )
 
+func main() {
+
+	var inputFile = "../../data/Paulius_Ratkevicius_L1_dat_1.json"
+	var users Users = ParseJson(inputFile)
+
+	inputBuff := make(chan User)
+	inputToMiddle := make(chan User)
+	middleToOutput := make(chan User)
+	resultBuff := make(chan Users)
+	LITERALLYWORKERGROUPS := make(chan bool)
+
+	go inputControl(inputBuff, inputToMiddle)
+	go outputControl(middleToOutput, resultBuff)
+
+	for i := 0; i < threadCount; i++ {
+		go worker(inputToMiddle, middleToOutput, LITERALLYWORKERGROUPS)
+	}
+
+	for users.Len() > 0 {
+		inputBuff <- users.removeLast()
+	}
+	close(inputBuff)
+	i := 0
+	for i < threadCount {
+		<-LITERALLYWORKERGROUPS
+		i++
+	}
+	close(middleToOutput)
+
+	users2 := <-resultBuff
+
+	output, _ := json.MarshalIndent(users2, " ", "		")
+	outputString := string(output)
+
+	reg := regexp.MustCompilePOSIX(`dat_.\.json`)
+
+	outputFile := reg.ReplaceAllString(inputFile, "resL2.json")
+
+	err := WriteToFile(outputFile, outputString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
 // Parses Json information and returns it in a Users sturct format
 func ParseJson(inputFile string) Users {
 	jsonFile, err := os.Open(inputFile)
@@ -42,44 +87,6 @@ func ParseJson(inputFile string) Users {
 	json.Unmarshal(byteValue, &users)
 
 	return users
-}
-
-func main() {
-
-	var inputFile = "../../data/Paulius_Ratkevicius_L1_dat_1.json"
-	var users Users = ParseJson(inputFile)
-
-	inputBuff := make(chan User)
-	inputToMiddle := make(chan User)
-	middleToOutput := make(chan User)
-	resultBuff := make(chan Users)
-
-	go inputControl(inputBuff, inputToMiddle)
-	go outputControl(middleToOutput, resultBuff)
-
-	for i := 0; i < threadCount; i++ {
-		go worker(inputToMiddle, middleToOutput)
-	}
-
-	for users.Len() > 0 {
-		inputBuff <- users.removeLast()
-	}
-	close(inputBuff)
-
-	users2 := <-resultBuff
-
-	output, _ := json.MarshalIndent(users2, " ", "		")
-	outputString := string(output)
-
-	reg := regexp.MustCompilePOSIX(`dat_.\.json`)
-
-	outputFile := reg.ReplaceAllString(inputFile, "resL2.json")
-
-	err := WriteToFile(outputFile, outputString)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 }
 
 // Writes given data string to a file located in filename.
@@ -141,7 +148,7 @@ func inputControl(input <-chan User, output chan<- User) {
 		//Keeps reading until fills up to MaxSize/(Threadcount*2) or channel is closed and empty.
 		for arr.Len() < maxSize/(threadCount*2) && !readDone {
 			user, innerDone := <-input
-			if !innerDone {
+			if innerDone {
 				arr.addUser(user)
 			}
 			readDone = !innerDone
@@ -151,9 +158,7 @@ func inputControl(input <-chan User, output chan<- User) {
 			output <- arr.removeLast()
 		}
 		//If channel is closed, stops the loop.
-		if readDone {
-			done = readDone
-		}
+		done = readDone
 	}
 	close(output)
 }
@@ -166,23 +171,24 @@ func outputControl(input <-chan User, output chan<- Users) {
 	//Reads until channel is closed and empty.
 	for !done {
 		user, innerDone := <-input
-		arr.addUser(user)
+		if innerDone {
+			arr.addUser(user)
+		}
 		done = !innerDone
 	}
 	output <- arr
-	close(output)
 }
 
 //// This is a func of worker thread
 
-func worker(input <-chan User, output chan<- User) {
+func worker(input <-chan User, output chan<- User, catch chan<- bool) {
 	var done bool
 	for !done {
 		user, innerDone := <-input
-		if user.Age >= ageReq {
+		if user.Age >= ageReq && innerDone {
 			output <- user
 		}
 		done = !innerDone
 	}
-	close(output)
+	catch <- true
 }
