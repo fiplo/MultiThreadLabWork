84dc14e6b9f26823311c42f0165680522be31628
fiplo <pauliuz95@gmail.com>
2019-12-12 14:55:18 +0200

    switched to char

--- Lab3/PRatkevicius_Lab3a.cu
+++ Lab3/PRatkevicius_Lab3a.cu
@@ -11,9 +11,10 @@ using namespace std;
 
 const int ARRAY_SIZE = 50;
 const int CHAR_ARRAY_SIZE = 11;
-const int STRING_SIZE = 260;
+
+
 struct User {
-    string Name;
+    char Name;
     int Age;
     double Balance;
 };
@@ -32,9 +33,15 @@ void read(User users[], string fileName, int &count) {
 	}
 	while(!ifs.fail()){
 		ifs >> Name >> Age >> Balance;
-    users[i].Name = Name;
 		users[i].Age = Age;
 		users[i].Balance = Balance;
+    for (int j = 0; j < CHAR_ARRAY_SIZE;j++) {
+			if (Name[j] == 0) {
+				items[i].stringas[j] = 0;
+				break;
+			}
+			users[i].Name[j] = (char)Name[j];
+		}
     if( ifs.eof() ) break;
     i++;
 	}
@@ -45,9 +52,16 @@ __global__ void addition(User* input, User* output, size_t n, int step)
 {
 	int index = threadIdx.x;
 	for (int i = index; i < n; i += step) {
+    int iterator = 0;
 		output[index].Age += input[i].Age;
 		output[index].Balance += input[i].Balance;
-    output[index].Name += input[i].Name;
+    for (int j = 0; j < CHAR_ARRAY_SIZE; j++) {
+			if (input[i].Name[j] == 0) {
+				break;
+			}
+			output[index].Name[iterator] = input[i].Name[j];
+			iterator++;
+		}
 	}
 }
 int main() {
